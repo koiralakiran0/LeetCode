b5388b66744aa15bacc81c91df242b2617bd5e9a
Kiran Koirala <Koiralakiran0@gmail.com>
2021-04-25 00:05:07 -0400

    changed few things

--- Easy/IPDefrag/IpDefrag.java
+++ Easy/IPDefrag/IpDefrag.java
@@ -1,4 +1,4 @@
-package Easy;
+package Easy.IPDefrag;
 
 public class IpDefrag {
     public static void main(String[] args) {
--- Easy/PalindromeLinkedList/Solution.java
+++ Easy/PalindromeLinkedList/Solution.java
@@ -1,3 +1,5 @@
+package PalindromeLinkedList;
+
 import java.util.Stack;
 
 public class Solution {
